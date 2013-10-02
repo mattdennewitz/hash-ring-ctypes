07681696317b10b73a8b85ccc45e124c4be55e40
Matt Dennewitz <mattdennewitz@gmail.com>
2013-10-02 18:49:16 -0500

    add hash functions to import

--- hash_ring/__init__.py
+++ hash_ring/__init__.py
@@ -2,5 +2,9 @@ __author__ = 'Matt Dennewitz'
 __version__ = (1, 0, 0)
 
 
-from hash_ring import HashRing, HASH_RING_OK, HASH_RING_ERR
+from hash_ring import (
+    HashRing,
+    HASH_RING_OK, HASH_RING_ERR,
+    HASH_FUNCTION_MD5, HASH_FUNCTION_SHA1
+)
 
