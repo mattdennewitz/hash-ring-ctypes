b8234ae6fa9dc1d9bd25130122bdde7d2426f1e4
Matt Dennewitz <mattdennewitz@gmail.com>
2013-10-02 18:54:03 -0500

    new setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,28 @@
 #!/usr/bin/env python
 
-from distutils.core import setup
+from setuptools import setup
 
 
-setup(name='hash-ring-ctypes',
-      version='1.0.0',
-      description='Fast ctypes-based wrapper around libhashring',
-      author='Matt Dennewitz',
-      author_email='mattdennewitz@gmail.com',
-      url='https://github.com/mattdennewitz/hash-ring-ctypes/',
-      packages=['hash_ring'])
+long_desc = open('README.md', 'r').read()
+
+setup(
+    name="hash-ring-ctypes",
+    version='1.0.0',
+    description='A fast ctypes-based wrapper around libhashring',
+    author='Matt Dennewitz',
+    author_email='mattdennwitz@gmail.com',
+    long_description=long_desc,
+    classifiers=[
+        'Development Status :: 5 - Production/Stable',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        ],
+    keywords='hash ring',
+    url='https://github.com/mattdennewitz/hash-ring-ctypes/',
+    license='BSD',
+    packages=['hash_ring'],
+    zip_safe=False,
+)
